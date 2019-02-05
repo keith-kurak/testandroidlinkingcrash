4448aafdf49ea5c77310990b5c0aba0d269b949c
Keith <keith.kurak@gmail.com>
2019-02-05 12:03:09 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,95 @@
+import { Linking, WebBrowser } from 'expo';
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  state = {
+    redirectData: null,
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Redirect Example</Text>
+
+        <Button
+          onPress={this._openWebBrowserAsync}
+          title="Try openWebBrowserAsync()"
+        />
+        <View style={{ height: 10 }}/>
+        <Button
+          onPress={this._openAuthSesssionAsync}
+          title="Try openAuthSessionAsync()"
+        />
+        {this._maybeRenderRedirectData()}
+      </View>
+    );
+  }
+
+  _handleRedirect = event => {
+    WebBrowser.dismissBrowser();
+
+    let data = Linking.parse(event.url);
+
+    this.setState({ redirectData: data });
+  };
+
+  _openWebBrowserAsync = async () => {
+    try {
+      this._addLinkingListener();
+      let result = await WebBrowser.openBrowserAsync(
+        // We add `?` at the end of the URL since the test backend that is used
+        // just appends `authToken=<token>` to the URL provided.
+        `https://backend-xxswjknyfi.now.sh/?linkingUri=com.llamaluvr.testandroidlinkingcrash://oauthredirect?`
+      );
+      this._removeLinkingListener();
+      this.setState({ result });
+    } catch (error) {
+      alert(error);
+      console.log(error);
+    }
+  };
+
+  _openAuthSesssionAsync = async () => {
+    try {
+      let result = await WebBrowser.openAuthSessionAsync(
+        // We add `?` at the end of the URL since the test backend that is used
+        // just appends `authToken=<token>` to the URL provided.
+        `https://backend-xxswjknyfi.now.sh/?linkingUri=com.llamaluvr.testandroidlinkingcrash://oauthredirect?`
+      );
+    } catch (error) {
+      alert(error);
+      console.log(error);
+    }
+  };
+
+  _addLinkingListener = () => {
+    Linking.addEventListener('url', this._handleRedirect);
+  };
+
+  _removeLinkingListener = () => {
+    Linking.removeEventListener('url', this._handleRedirect);
+  };
+
+  _maybeRenderRedirectData = () => {
+    if (!this.state.redirectData) {
+      return;
+    }
+
+    return <Text>{JSON.stringify(this.state.redirectData)}</Text>;
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 40,
+  },
+  header: {
+    fontSize: 25,
+    marginBottom: 25,
+  },
+});
---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp756db71a4fa849eab0dde164561b89b7://192.168.1.67:19000";
+
+}
